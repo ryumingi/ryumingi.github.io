2c0ec4b0124c52c109b284b0ca7d896225d03eb6
mingiryu <mingi.ryu@icloud.com>
2023-04-13 00:01:50 -0400

    Update .tree.sh

--- .tree.sh
+++ .tree.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Generate tree representation of the directory w/ hyperlink
 tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
        sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
 
 printf "${tree}" > README.md
+
+sed '/README.md/d' README.md > README.md
