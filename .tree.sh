d401c12b1984f521175cf732d7cd2d02543a7f3b
mingiryu <mingi.ryu@icloud.com>
2023-04-26 02:31:00 -0400

    Update .tree.sh

--- .tree.sh
+++ .tree.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 source="[source](https://github.com/ryumingi)"
+bookmarks="[bookmarks](bookmarks.md]"
 
 # Generate tree representation of the directory w/ hyperlink
 tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
        sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
 
-printf "${source}\n${tree}" > README.md
+printf "${bookmarks}\n\n${source}\n\n${tree}" > README.md
 
 sed -i '/README.md/d' README.md
+sed -i '/bookmarks.md/d' README.md
